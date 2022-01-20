dc4ca0b2971f104e8ca21e3c825f4e5e659c8c31
Pavel Orlovskyi <Lorengreyn@gmail.com>
2022-01-20 20:13:07 +0200

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector(`.gallery`);
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => { 
+  return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+  })
+    .join("");
+  
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryList.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target !== event.currentTarget) {
+        const img = event.target.dataset.source;
+
+        const modal = basicLightbox.create(`
+    <img src="${img}" width="1280">
+`);
+        
+        modal.show();
+
+        galleryList.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        modal.close();
+      }
+    });
+  }
+});
+
 console.log(galleryItems);
